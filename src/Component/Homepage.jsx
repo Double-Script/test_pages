b527a75dce7983e896b7eadf76e995629308e789
vedant patil <patilvedantraj@gmail.com>
2025-01-02 11:48:54 +0530

    new test

---
+++ src/Component/Homepage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <header className="header">
+        <h1>Welcome to Our Domain</h1>
+        <p>This domain is reserved for Varad and Ved for testing purposes.</p>
+      </header>
+      <main className="content">
+        <p>Feel free to explore and test your ideas here!</p>
+      </main>
+      <footer className="footer">
+        <p>&copy; 2025 Varad and Ved. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default HomePage;
